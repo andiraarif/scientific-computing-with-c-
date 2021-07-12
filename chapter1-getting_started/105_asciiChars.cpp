ad4fd8df454539ff080aab08bc77e8fedb91c4ed
Mohamad Arif Andira <arifandira0209@gmail.com>
2021-07-12 10:38:39 +0700

    Created 105_asciiChars.cpp

---
+++ chapter1-getting_started/105_asciiChars.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    char letter;
+    letter = 'a'; // note the single quotation marks
+
+    std::cout << "The character is " << letter << "\n";
+
+    return 0;
+}
